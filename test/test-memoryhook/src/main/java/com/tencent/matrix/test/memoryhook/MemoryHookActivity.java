c6124d17c7a459275c5af0e4206132eb9a8181ca
杨荣 <yangrong@ewjh.cn>
2022-11-25 18:44:07 +0800

    no message

--- test/test-memoryhook/src/main/java/com/tencent/matrix/test/memoryhook/MemoryHookActivity.java
+++ test/test-memoryhook/src/main/java/com/tencent/matrix/test/memoryhook/MemoryHookActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.tencent.matrix.backtrace.WarmUpReporter;
 import com.tencent.matrix.backtrace.WeChatBacktrace;
